6b864fc571d002088f8961f4e1f73bcea61691f9
Himanshu Yadav <himanshu07319@gmail.com>
2023-10-07 17:09:26 -0500

    added flops to mac input and output

--- src/mac_unit.v
+++ src/mac_unit.v
@@ -11,7 +11,40 @@ module mac_unit (
     reg [15:0] sum;
     reg val;
 
-    assign c = sum;
+    reg [7:0] a_flop_out;
+    reg [7:0] b_flop_out;
+    reg [7:0] out_ff;
+    // Flop for port a
+    always @(posedge clk) begin
+        if (!reset_n) 
+            a_flop_out <= 8'b0000_0000;
+        else if (ena)
+            a_flop_out <= a;
+        else
+            a_flop_out <= a_flop_out;
+    end
+
+    //Flop for port b
+     always @(posedge clk) begin
+        if (!reset_n) 
+            b_flop_out <= 8'b0000_0000;
+        else if (ena)
+            b_flop_out <= b;
+        else
+            b_flop_out <= b_flop_out;
+    end
+
+    //Flop for output
+    always @(posedge clk) begin
+        if (!reset_n) 
+            out_ff <= 8'b0000_0000;
+        else if (ena)
+            out_ff <= sum;
+        else
+            out_ff <= out_ff;
+    end
+    //-------------------------------
+    assign c = out_ff;
     assign valid = val;
 
     always @(posedge clk) begin
@@ -19,7 +52,7 @@ module mac_unit (
             sum <= 16'b0000_0000_0000_0000;
             val <= 1'b0;
         end else if (enable) begin
-            sum <= sum + (a*b);
+            sum <= sum + (a_flop_out*b_flop_out);
             val <= 1'b1;
         end else begin
             sum <= 16'b0000_0000_0000_0000;
